249dfc374be4685c45cf2f3fafbb8d32e491e348
Firmino Changani <firmino.changani@gmail.com>
2023-01-01 16:44:40 +0000

    replace os.Getenv with config util
    - update port mapping

--- internal/adapters/psql_read_user_repo_test.go
+++ internal/adapters/psql_read_user_repo_test.go
@@ -2,11 +2,11 @@ package adapters_test
 
 import (
 	"context"
-	"database/sql"
-	"os"
 	"testing"
 	"users-service-cqrs/internal/adapters"
 	"users-service-cqrs/internal/app/query"
+	"users-service-cqrs/internal/common/config"
+	"users-service-cqrs/internal/common/psql"
 
 	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
@@ -15,10 +15,8 @@ import (
 func TestPsqlReadUserRepo_FindAll(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	db, err := sql.Open("postgres", os.Getenv("GOOSE_DBSTRING"))
-	if err != nil {
-		panic(err)
-	}
+	cfg := config.New()
+	db := psql.Connect(cfg.PsqlUri)
 
 	repo := adapters.NewPsqlReadUserRepo(db)
 	userList, err := repo.FindAll(ctx, query.AllUsers{Status: "blocked"})
